d27cab1e9002bc124df42d0e221727ac7117ea75
xuwuh <lan.mao.kl@gmail.com>
2026-05-23 02:56:54 +0700

    обновлен main и сборка

--- bild.bat
+++ bild.bat
@@ -1,6 +1,7 @@
 gcc ^
 c\main.c ^
 c\config.c ^
+c\load_dataset.c ^
 -o nern.exe ^
 -lm
 
--- c/main.c
+++ c/main.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
 
 #include "../include/config.h"
+#include "../include/load_dataset.h"
 
 int main(){
     Config config;
+    Dataset train_dataset={0};
 
-    load("configs/config.txt", &config);
+    load("configs/config.txt", &config); //загружаем конфиг
+
+    dataset_load(&train_dataset, config.train_data_path, &config, 100);
+   
+    dataset_print(&train_dataset);
+    dataset_print_labels(&train_dataset, 10);
+    dataset_print_pixels(&train_dataset, 10);
+
+    //тесты 
     conf_print(&config);
+
+    dataset_free(&train_dataset);
 
     return 0;
 }
